ab87c433de7705aadad781eeb808fea87379625d
Xu Deng <d8660091@gmail.com>
2018-06-29 16:52:23 -0400

    Do not sort buffers when search

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,7 @@
  '(dired-listing-switches "-aBl --group-directories-first")
  '(display-line-numbers-widen t)
  '(elpy-rpc-timeout 10)
+ '(enable-local-variables :all)
  '(evil-mode-line-format '(after . mode-line-front-space))
  '(evil-want-C-u-scroll nil)
  '(exec-path
@@ -60,7 +61,7 @@
  '(org-agenda-files nil)
  '(org-clock-persist t)
  '(package-selected-packages
-   '(lsp-mode csv-mode elm-mode racer rust-mode org-jira gist elpy flow-minor-mode rg prettier-js dockerfile-mode diminish counsel-projectile fzf rjsx-mode go-rename company-go delight sass-mode mustache-mode yaml-mode evil-matchit evil-mc helm php-mode js2-mode company-jedi go-eldoc counsel sr-speedbar cider dired+ paredit company tide pug-mode fuzzy swiper-helm haskell-mode clojure-mode tern evil-numbers ace-link auctex rainbow-mode helm-ag anzu flycheck go-mode transpose-frame markdown-mode wgrep exec-path-from-shell ag helm-dash avy restclient magit emmet-mode which-key yasnippet ivy key-chord evil-leader evil-nerd-commenter evil-surround evil helm-projectile projectile editorconfig git-gutter-fringe web-mode use-package))
+   '(elpy lsp-mode csv-mode elm-mode racer rust-mode org-jira gist flow-minor-mode rg prettier-js dockerfile-mode diminish counsel-projectile fzf rjsx-mode go-rename company-go delight sass-mode mustache-mode yaml-mode evil-matchit evil-mc helm php-mode js2-mode company-jedi go-eldoc counsel sr-speedbar cider dired+ paredit company tide pug-mode fuzzy swiper-helm haskell-mode clojure-mode tern evil-numbers ace-link auctex rainbow-mode helm-ag anzu flycheck go-mode transpose-frame markdown-mode wgrep exec-path-from-shell ag helm-dash avy restclient magit emmet-mode which-key yasnippet ivy key-chord evil-leader evil-nerd-commenter evil-surround evil helm-projectile projectile editorconfig git-gutter-fringe web-mode use-package))
  '(powerline-default-separator 'arrow)
  '(projectile-enable-caching t)
  '(projectile-other-file-alist
@@ -86,7 +87,7 @@
      ("lock" "")
      ("gpg" "")))
  '(recentf-exclude
-   '("/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\)\\'" "/.emacs.d/elpa"))
+   '("/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\)\\'" "/.emacs.d/elpa") t)
  '(recentf-max-menu-items 2000)
  '(recentf-max-saved-items 1000)
  '(safe-local-variable-values
@@ -124,8 +125,8 @@
  '(web-mode-enable-css-colorization t)
  '(web-mode-extra-keywords '(("javascript" "namespace" "type")))
  '(web-mode-markup-indent-offset 2)
- '(web-mode-script-padding 0)
- '(web-mode-style-padding 0))
+ '(web-mode-script-padding 0 t)
+ '(web-mode-style-padding 0 t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -619,7 +620,7 @@
     (when (eq (string-match "\~.*\~$" (buffer-name)) nil)
       (elpy-mode 1)
       (flycheck-mode -1)
-      (evil-local-set-key 'normal (kbd "C-c C-j") 'elpy-goto-definition)))
+      (evil-local-set-key 'normal (kbd "g d") 'elpy-goto-definition)))
   (elpy-enable)
   (remove-hook 'python-mode-hook 'elpy-mode)
   (add-hook 'python-mode-hook 'my-elpy-mode))
@@ -687,3 +688,8 @@ If `universal-argument' is called first, copy only the dir path."
                  buffer))
              buffer-list)))
 (advice-add 'helm-skip-boring-buffers :filter-return 'my-filter-dired-buffers)
+
+
+(defun my-helm-buffers-sort-transformer (_ candidates source)
+  candidates)
+(advice-add 'helm-buffers-sort-transformer :around 'my-helm-buffers-sort-transformer)
